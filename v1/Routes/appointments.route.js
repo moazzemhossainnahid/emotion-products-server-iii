9de1541df5b73478352b76a8232f3d23fd8c9e7c
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-08-05 16:45:04 +0600

    resolved

--- v1/Routes/appointments.route.js
+++ v1/Routes/appointments.route.js
@@ -1,8 +1,6 @@
 const express = require("express");
 const appointmentController = require("../Controllers/appointments.controller");
 const verifyToken = require("../Middlewares/verifyToken");
-const uploader = require("../Middlewares/Uploader");
-const multer  = require('multer')
 const router = express.Router();
 
 
--- v1/Routes/products.route.js
+++ v1/Routes/products.route.js
@@ -1,13 +1,11 @@
 const express = require("express");
 const productController = require("../Controllers/products.controller");
 const verifyToken = require("../Middlewares/verifyToken");
-const uploader = require("../Middlewares/Uploader");
-const multer  = require('multer')
 const router = express.Router();
 
 
 // publish a product
-router.post("/", uploader.single("resource"), productController.publishAProduct);
+router.post("/", productController.publishAProduct);
 
 // get all products
 router.get("/", productController.getAllproducts);
