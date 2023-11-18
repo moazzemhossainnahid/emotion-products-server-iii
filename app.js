c1c821def7762c32c2789eceef8dcdeb03040ad6
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-11-18 10:18:38 +0600

    resolved

--- app.js
+++ app.js
@@ -5,13 +5,27 @@ const PORT = process.env.PORT || 5000;
 const colors = require("colors");
 const mongoose = require('mongoose');
 require('dotenv').config();
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 
 
 app.use(cors({ origin: '*', methods: 'GET,HEAD,PUT,PATCH,POST,DELETE' }));
 app.use(express.json());
 
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:5173');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
 
+app.use(
+    '/api',
+    createProxyMiddleware({
+        target: 'https://emotion-products-server-iii.vercel.app',
+        changeOrigin: true,
+    })
+);
 
 app.use('/public', express.static('public'))
 
