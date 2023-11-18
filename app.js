1f5c3e427ca1e2a19bb1c859f83f09dd5c0aee7b
moazzemhossainnahid <moazzemhossainnahid@gmail.com>
2023-11-18 10:28:12 +0600

    resolved

--- app.js
+++ app.js
@@ -5,7 +5,6 @@ const PORT = process.env.PORT || 5000;
 const colors = require("colors");
 const mongoose = require('mongoose');
 require('dotenv').config();
-const { createProxyMiddleware } = require('http-proxy-middleware');
 
 
 
